509c3e4f9d6d2d048d8cba29274c8ad3dddf57d9
jowy99 <jowycarreras@gmail.com>
2025-04-10 11:05:55 +0200

    Eliminar rutina

--- src/controllers/rutinas.controller.js
+++ src/controllers/rutinas.controller.js
@@ -168,9 +168,38 @@ const editarRutina = async (req, res) => {
     }
 };
 
+const eliminarRutina = async (req, res) => {
+    const usuario_id = req.usuario?.id;
+    const { id } = req.params;
+  
+    if (!usuario_id) {
+      return res.status(401).json({ error: 'No autorizado' });
+    }
+  
+    try {
+      const rutinaRes = await pool.query(`
+        SELECT * FROM rutinas
+        WHERE id = $1 AND usuario_id = $2
+      `, [id, usuario_id]);
+  
+      if (rutinaRes.rowCount === 0) {
+        return res.status(404).json({ error: 'Rutina no encontrada o no autorizada' });
+      }
+  
+      await pool.query(`DELETE FROM rutinas WHERE id = $1`, [id]);
+  
+      res.json({ mensaje: 'Rutina eliminada correctamente' });
+  
+    } catch (error) {
+      console.error('❌ Error al eliminar la rutina:', error.message);
+      res.status(500).json({ error: 'Error al eliminar la rutina' });
+    }
+};
+
 module.exports = {
     crearRutina,
     obtenerRutinas,
     obtenerRutinaPorId,
-    editarRutina
+    editarRutina,
+    eliminarRutina
 };
--- src/routes/rutinas.routes.js
+++ src/routes/rutinas.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
-const { crearRutina, obtenerRutinas, obtenerRutinaPorId, editarRutina } = require('../controllers/rutinas.controller');
+const { crearRutina, obtenerRutinas, obtenerRutinaPorId, editarRutina, eliminarRutina } = require('../controllers/rutinas.controller');
 
 // Ruta para crear una rutina (requiere estar autenticado)
 router.post('/', authMiddleware, crearRutina);
 router.get('/', authMiddleware, obtenerRutinas);
 router.get('/:id', authMiddleware, obtenerRutinaPorId);
-router:this.put('/:id', authMiddleware, editarRutina);
+router.put('/:id', authMiddleware, editarRutina);
+router.delete('/:id', authMiddleware, eliminarRutina);
 
 module.exports = router;
