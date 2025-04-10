35d19e3a38408012f800d3af9c0a454d52c7c9db
jowy99 <jowycarreras@gmail.com>
2025-04-10 11:00:17 +0200

    Obtener rutinas controller

--- src/routes/rutinas.routes.js
+++ src/routes/rutinas.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { crearRutina } = require('../controllers/rutinas.controller');
 const authMiddleware = require('../middleware/authMiddleware');
+const { crearRutina, obtenerRutinas } = require('../controllers/rutinas.controller');
 
 // Ruta para crear una rutina (requiere estar autenticado)
 router.post('/', authMiddleware, crearRutina);
+router.get('/', authMiddleware, obtenerRutinas);
 
 module.exports = router;
