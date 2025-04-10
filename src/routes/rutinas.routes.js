c534e0665f3a390b1ebfddcbfbcd98486e9400b8
jowy99 <jowycarreras@gmail.com>
2025-04-10 11:03:47 +0200

    Obtener rutina opr ID

--- src/routes/rutinas.routes.js
+++ src/routes/rutinas.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
-const { crearRutina, obtenerRutinas } = require('../controllers/rutinas.controller');
+const { crearRutina, obtenerRutinas, obtenerRutinaPorId } = require('../controllers/rutinas.controller');
 
 // Ruta para crear una rutina (requiere estar autenticado)
 router.post('/', authMiddleware, crearRutina);
 router.get('/', authMiddleware, obtenerRutinas);
+router.get('/:id', authMiddleware, obtenerRutinaPorId);
 
 module.exports = router;
