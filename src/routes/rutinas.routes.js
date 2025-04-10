5ae4f40c482373d98f4f4223a631a1216ff52942
jowy99 <jowycarreras@gmail.com>
2025-04-10 11:04:44 +0200

    Editar rutina

--- src/routes/rutinas.routes.js
+++ src/routes/rutinas.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
-const { crearRutina, obtenerRutinas, obtenerRutinaPorId } = require('../controllers/rutinas.controller');
+const { crearRutina, obtenerRutinas, obtenerRutinaPorId, editarRutina } = require('../controllers/rutinas.controller');
 
 // Ruta para crear una rutina (requiere estar autenticado)
 router.post('/', authMiddleware, crearRutina);
 router.get('/', authMiddleware, obtenerRutinas);
 router.get('/:id', authMiddleware, obtenerRutinaPorId);
+router:this.put('/:id', authMiddleware, editarRutina);
 
 module.exports = router;
