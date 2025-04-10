2e01ffdbcf04c8ed394bb2ca3e107dac8d3e275a
jowy99 <jowycarreras@gmail.com>
2025-04-10 11:28:29 +0200

    Ver progreso de un ejercicio

--- src/routes/progreso.routes.js
+++ src/routes/progreso.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { registrarProgreso } = require('../controllers/progreso.controller');
 const authMiddleware = require('../middleware/authMiddleware');
+const { registrarProgreso, verProgresoPorEjercicio } = require('../controllers/progreso.controller');
 
 router.post('/', authMiddleware, registrarProgreso);
+router.get('/ejercicio/:rutina_ejercicio_id', authMiddleware, verProgresoPorEjercicio);
 
 module.exports = router;
