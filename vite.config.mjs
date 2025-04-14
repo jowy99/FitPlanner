9dfdfbdbf9fd7d5b418f921c421a1c4f8946a5c8
jowy99 <jowycarreras@gmail.com>
2025-04-14 18:46:17 +0200

    Rutas limpias

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
@@ -7,6 +8,17 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@components': path.resolve(__dirname, './src/components'),
+      '@layouts': path.resolve(__dirname, './src/layouts'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@context': path.resolve(__dirname, './src/context'),
+    },
+  },
   root: './src',
   build: {
     outDir: '../dist',
